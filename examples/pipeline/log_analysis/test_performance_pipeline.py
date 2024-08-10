403ed5373554aee906f533c8b9f1c822bfd7851b
settor21 <43499539+settor21@users.noreply.github.com>
2024-08-10 06:59:38 +0100

    batch size update

--- examples/pipeline/log_analysis/test_performance_pipeline.py
+++ examples/pipeline/log_analysis/test_performance_pipeline.py
@@ -8,8 +8,7 @@ import seaborn as sns
 
 # Define the batch sizes
 BATCH_SIZES = [10, 100, 750, 5000, 10000,
-               50000, 100000, 200000, 500000, 1000000, 2000000, 4500000, 10000000, 17500000, 30000000, 50000000,
-               75000000, 100000000, 250000000, 500000000, 85000000, 1000000000]
+               50000, 100000, 200000, 500000, 1000000, 2000000, 5000000]
 
 # Define the output CSV file
 CSV_FILE = "/results/pipeline_performance_results.csv"
